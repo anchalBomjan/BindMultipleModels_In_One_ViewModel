477e5433401215f99ec0a66f96ee615344f5879e
PERCOID-IT <PERCOID-IT@DESKTOP-SANISHA>
2024-06-24 16:51:10 +0545

    Some changes in Course Controller#

--- BindMultipleModels_In_One_ViewModel.Web/Controllers/CourseController.cs
+++ BindMultipleModels_In_One_ViewModel.Web/Controllers/CourseController.cs
@@ -2,98 +2,117 @@
 using BindMultipleModels_In_One_ViewModel.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System;
+using System.Threading.Tasks;
 
 namespace BindMultipleModels_In_One_ViewModel.Web.Controllers
 {
-	public class CourseController : Controller
-	{
+    public class CourseController : Controller
+    {
+        private readonly ApplicationDbContext _context;
 
+        public CourseController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
 
+        // GET: /Course/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
 
-		private readonly ApplicationDbContext _context;
+        // POST: /Course/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(CourseViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(viewModel);
+            }
 
-		public CourseController(ApplicationDbContext context)
-		{
-			_context = context;
-		}
-
-		// GET: /Course/Create
-		public IActionResult Create()
-		{
-			return View();
-		}
-		// POST: /Course/Create
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public async Task< IActionResult> Create(CourseViewModel viewModel)
-		{
             var course = new Course
             {
                 CourseName = viewModel.CourseName,
                 CourseDescription = viewModel.CourseDescription,
                 Credits = viewModel.Credits
-                
             };
-			await _context.Courses.AddAsync(course);
-			await _context.SaveChangesAsync();
-			return RedirectToAction("Index","Course");
+            await _context.Courses.AddAsync(course);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
         }
 
-
+        // GET: /Course/Index
         [HttpGet]
-        
-
-        public IActionResult Index()
-		{
-			var courses = _context.Courses.ToList();
-			return View(courses);
-		}
-
-
-
-        [HttpGet]
-        public async Task<IActionResult> Edit(int CourseID)
+        public async Task<IActionResult> Index()
         {
-            var course = await _context.Courses.FindAsync( CourseID);
-            return View(course);
+            var courses = await _context.Courses.ToListAsync();
+            return View(courses);
         }
 
-        [HttpPost]
-        public async Task<IActionResult> Edit(Course viewModel)
+        // GET: /Course/Edit/{id}
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
         {
-            var course = await _context.Courses.FindAsync(viewModel.CourseID);
-
-            if ( course is not null)
+            if (id <= 0)
             {
-                course.CourseName = viewModel.CourseName;
-                course.CourseDescription = viewModel.CourseDescription;
-                course.Credits = viewModel.Credits;
-
-
-                await _context.SaveChangesAsync();
+                return View("Error", new ErrorViewModel { RequestId = "Invalid Course ID" });
             }
 
-            return RedirectToAction("Index ", "Course");
-
-
-        }
-
-
-        [HttpPost]
-        public async Task<IActionResult> Delete(Course viewModel)
-        {
-            var course = await _context.Courses
-                .AsNoTracking()
-                .FirstOrDefaultAsync(x => x.CourseID == viewModel.CourseID);
-            if (course is not null)
+            var course = await _context.Courses.FindAsync(id);
+            if (course == null)
             {
-                _context.Courses.Remove(viewModel);
-                await _context.SaveChangesAsync();
-
+                return View("Error", new ErrorViewModel { RequestId = $"No course found with ID {id}" });
             }
-            return RedirectToAction("Index", "Course");
+
+            var viewModel = new CourseViewModel
+            {
+                CourseName = course.CourseName,
+                CourseDescription = course.CourseDescription,
+                Credits = course.Credits
+            };
+
+            return View(viewModel);
         }
 
+        // POST: /Course/Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, CourseViewModel viewModel)
+        {
+            if (id <= 0 || !ModelState.IsValid)
+            {
+                return View(viewModel);
+            }
+
+            var course = await _context.Courses.FindAsync(id);
+            if (course == null)
+            {
+                return View("Error", new ErrorViewModel { RequestId = $"No course found with ID {id}" });
+            }
+
+            course.CourseName = viewModel.CourseName;
+            course.CourseDescription = viewModel.CourseDescription;
+            course.Credits = viewModel.Credits;
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        // POST: /Course/Delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var course = await _context.Courses.FindAsync(id);
+            if (course == null)
+            {
+                return View("Error", new ErrorViewModel { RequestId = $"No course found with ID {id}" });
+            }
+
+            _context.Courses.Remove(course);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
