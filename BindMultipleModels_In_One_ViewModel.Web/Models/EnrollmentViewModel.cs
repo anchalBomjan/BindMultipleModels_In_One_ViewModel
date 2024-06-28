3e6855e6edb25de810780b82bc237fc558bc9d76
PERCOID-IT <PERCOID-IT@DESKTOP-SANISHA>
2024-06-28 17:27:50 +0545

    Adding  EnrollmentViewsModel#

---
+++ BindMultipleModels_In_One_ViewModel.Web/Models/EnrollmentViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BindMultipleModels_In_One_ViewModel.Web.Models
+{
+    public class EnrollmentViewModel
+    {
+
+        public int EnrollmentID { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string StudentID { get; set; }
+
+        [Required]
+        public int CourseID { get; set; }
+
+        [Required]
+        public DateTime EnrollmentDate { get; set; }
+
+        // Lists of students and courses for dropdowns
+        public IEnumerable<Student> Students { get; set; }
+        public IEnumerable<Course> Courses { get; set; }
+    }
+}
